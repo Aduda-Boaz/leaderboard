f047cec7fc6809c0b78d53bd67789b5b7ebb86da
Aduda-Boaz <adudaboaz@gmail.com>
2022-02-04 20:50:41 +0300

    Modify the files to fix load error

--- src/index.js
+++ src/index.js
@@ -5,15 +5,20 @@ import { createScoreListElement, appendListElement, clearForms } from './modules
 const addScoreList = document.getElementById('add');
 const refreshScoreList = document.getElementById('refresh');
 
+const addToLocalStorage = (scores) => {
+  localStorage.setItem('scores', JSON.stringify(scores));
+};
+
 addScoreList.addEventListener('click', () => {
   const newName = document.getElementById('user').value;
-  const newScore = document.getElementById('user').value;
+  const newScore = document.getElementById('score').value;
 
   setAPIScores(newName, newScore);
 
   const listElement = createScoreListElement(newName, newScore);
 
   appendListElement(listElement);
+  addToLocalStorage();
   clearForms();
 });
 
