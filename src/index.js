c5ef831afb3d87497519ea281cfc539e0c871d6b
Aduda-Boaz <adudaboaz@gmail.com>
2022-02-05 11:38:24 +0300

    Fix the display and refesh problem

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import './style.css';
-import { score } from './modules/hit-api.js';
-import { newScoreList } from './modules/scores.js';
+import apiData from './modules/hit-api.js';
+import {
+  nameInput, refreshBtn, scoreInput, submitBtn,
+} from './modules/scores.js';
 
-window.onload = () => {
-  score.getAPIScores();
-  newScoreList.addScoreToList();
-};
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/DIvC379OvteODU5bAfDN/scores';
+
+submitBtn.addEventListener('click', async (e) => {
+  e.preventDefault();
+  await apiData(url, { user: nameInput.value, score: scoreInput.value });
+  nameInput.value = '';
+  scoreInput.value = '';
+  const msg = document.getElementById('alart');
+  setTimeout(() => { msg.innerHTML = 'Score added successfully'; }, 1000);
+  setTimeout(() => { msg.innerHTML = ''; }, 4000);
+});
+
+const listItems = document.getElementsByClassName('list')[0];
+
+async function showScores() {
+  listItems.innerHTML = '';
+  const { result: scores } = await fetch(url)
+    .then((resp) => resp.json());
+  scores.forEach((result) => {
+    const item = document.createElement('li');
+    item.innerHTML = `${result.user}: ${result.score}`;
+    listItems.appendChild(item);
+  });
+}
+
+refreshBtn.addEventListener('click', showScores);
+window.onload = showScores;
--- src/modules/hit-api.js
+++ src/modules/hit-api.js
@@ -1,44 +1,15 @@
-export default class Score {
-  constructor() {
-    this.url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/DIvC379OvteODU5bAfDN/scores';
-    this.listScore = document.querySelector('.list');
-  }
-
-  getAPIScores = async () => {
-    const response = await fetch(this.url);
-
-    const resObj = await response.json();
-    this.showListElement(resObj.result);
-  };
-
-  setAPIScores = async (newName, newScore) => {
-    await fetch(this.url, {
-      method: 'POST',
-      body: JSON.stringify({
-        user: newName,
-        score: newScore,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
-  };
-
-  showListElement = (array) => {
-    array.sort((a, b) => b.score - a.score);
-    array.forEach((element) => {
-      const lib = document.createElement('li');
-      lib.className = ('list');
-      const par1 = document.createElement('p');
-      par1.innerText = `${element.user}`;
-      par1.className = ('user');
-      lib.appendChild(par1);
-      const par2 = document.createElement('p');
-      par1.innerText = `${element.score}`;
-      par1.className = ('score');
-      lib.appendChild(par2);
-    });
-  }
+export default async function apiData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data),
+  });
+  return response.json();
 }
-
-export const score = new Score();
